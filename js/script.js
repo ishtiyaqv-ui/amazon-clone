18cb3018499311ef10f44da546e3bd9faef5bb0f
ishtiyaqv-ui <ishtiyaq.v@gmail.com>
2026-07-14 10:20:44 +0000

    Created ShopEasy homepage

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let buttons=document.querySelectorAll(".card button");
+
+buttons.forEach(function(button){
+
+button.addEventListener("click",function(){
+
+alert("Product added to cart!");
+
+});
+
+});
